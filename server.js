4d43a46bcf881ccaf9f581a3eec83acd701320e2
Dmitry Tsydenov <rondimts@gmail.com>
2017-01-31 13:01:35 +0700

    dsfsdf

--- server.js
+++ server.js
@@ -6,17 +6,20 @@ const app = express();
 
 console.log(process.env.NODE_ENV);
 
-if (process.env.NODE_ENV !== 'production') {
-  const webpackMiddleware = require('webpack-dev-middleware');
-  const webpack = require('webpack');
-  const webpackConfig = require('./webpack.config.js');
+app.use(express.static('dist'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
 
-  app.use(webpackMiddleware(webpack(webpackConfig)));
-} else {
-  app.use(express.static(__dirname + '/dist'));
-  app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
-  });
-}
 
 app.listen(process.env.PORT || 3050, () => console.log('Listening'));
+
+
+// if (process.env.NODE_ENV !== 'production') {
+//   const webpackMiddleware = require('webpack-dev-middleware');
+//   const webpack = require('webpack');
+//   const webpackConfig = require('./webpack.config.js');
+//
+//   app.use(webpackMiddleware(webpack(webpackConfig)));
+// } else {
+// }
