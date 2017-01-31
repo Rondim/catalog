b0e6a7da1c9dbd66bbc6ae6c79b520d277464d8e
Dmitry Tsydenov <rondimts@gmail.com>
2017-01-31 12:41:33 +0700

    some modifications

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ if (process.env.NODE_ENV !== 'production') {
   app.use(webpackMiddleware(webpack(webpackConfig)));
 } else {
   app.use(express.static(path.join(__dirname, '/dist')));
-  app.get('*', (req, res) => {
+  app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist/index.html'));
   });
 }
