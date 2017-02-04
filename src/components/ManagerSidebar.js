1bf7981f42fd3c6d8ac5254a3fbc2677efbeb211
Dmitry Tsydenov <rondimts@gmail.com>
2017-02-04 21:10:19 +0700

    added some consts

--- src/components/ManagerSidebar.js
+++ src/components/ManagerSidebar.js
@@ -4,18 +4,11 @@ class ManagerSideBar extends Component {
   render() {
     return (
       <div className="manager_sidebar text-center">
-
             <div className="btn-group-vertical btn-group-lg department_manufacturer" role="group">
               <button type="button" className="btn btn-default">Отдел</button>
               <button type="button" className="btn btn-default">Производитель</button>
+              <button type="button" className="btn btn-default">Металл</button>
             </div>
-            <div className="btn-group metals" role="group">
-              <div className="text-center">
-                <button type="button" className="btn btn-default">Au</button>
-                <button type="button" className="btn btn-default">Ag</button>
-              </div>
-            </div>
-
       </div>
     );
   }
