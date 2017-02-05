dd39ec1c3c8644f1572560ecfce6fecfb8837b3d
xax <lueurxax@gmail.com>
2017-02-05 18:05:51 +0300

    added state for popup, show state on the button

--- src/components/ManagerSidebar.js
+++ src/components/ManagerSidebar.js
@@ -6,32 +6,56 @@ class ManagerSideBar extends Component {
   constructor (props){
     super(props);
     this.state = { 
-      isActivePopup:{
-          department: false
+      isActivePopup:'',
+      selectedFilter: {
+          department: {},
+          manufacturer: {}
       }
     };
-    this.handleFilterClick = this.handleFilterClick.bind(this);
+    this.handleFilterEnter = this.handleFilterEnter.bind(this);
+    this.handleFilterLeave = this.handleFilterLeave.bind(this);
+    this.handleFilterSelect = this.handleFilterSelect.bind(this);
   }
-  handleFilterClick(e){
+  handleFilterEnter(e){
     const filterName = e.target.name;
-    const filterItems = base.filters[filterName];
     //Дальше напишем работу со state
     
-    this.setState((PrevState)=>{
-        let isActivePopup={};
-        isActivePopup[filterName]= PrevState.isActivePopup[filterName] ? false:true;
-      return {isActivePopup:isActivePopup};
-    });
+    this.setState({isActivePopup: filterName});
+  }
+    handleFilterLeave(e){
+        const filterName = e.target.name;
+        //Дальше напишем работу со state
+
+        this.setState({isActivePopup: false});
+    }
+  handleFilterSelect(e){
+      const id = e.target.id;
+      this.setState(PrevState =>{
+          let selectedFilterVal = PrevState.selectedFilter[this.state.isActivePopup];
+          selectedFilterVal[id]= selectedFilterVal[id] ? false:true;
+          let selectedFilter=PrevState.selectedFilter;
+          selectedFilter[this.state.isActivePopup]=(selectedFilterVal);
+          return{selectedFilter};
+      });
+      console.log(this.state.selectedFilter);
   }
   renderButtons(filterName){
       const filterItems = base.filters[filterName];
       return Object.keys(filterItems).map((key)=> {
           const item = filterItems[key].name;
-          return (<Button key={key}>{item}</Button>)
+          return (<Button
+              name={filterName}
+              onClick={this.handleFilterSelect}
+              key={key}
+              id={key}
+              bsStyle={this.state.selectedFilter[filterName][key] ? "success":"primary"}
+              onMouseEnter={this.handleFilterEnter}
+              onMouseLeave={this.handleFilterLeave}
+          >{item}</Button>)
       });
   }
   renderFilter(filterName){
-    return (this.state.isActivePopup[filterName] ?
+    return (this.state.isActivePopup==filterName ?
           <Col className="btn-group-vertical btn-group-lg negativeMargin" role="group"  lg={7} md={9}>
               {this.renderButtons(filterName)}
           </Col>:<Col  lg={7} md={7} className="negativeMargin"></Col>
@@ -41,9 +65,9 @@ class ManagerSideBar extends Component {
     return (
       <div className="manager_sidebar text-center">
         <div className="btn-group-vertical btn-group-lg department_manufacturer" role="group">
-          <Button name="department" onClick={this.handleFilterClick}>Отдел</Button>
+          <Button name="department" onMouseEnter={this.handleFilterEnter} onMouseLeave={this.handleFilterLeave}>Отдел</Button>
           {this.renderFilter('department')}
-          <Button name="manufacturer" onClick={this.handleFilterClick}>Производитель</Button>
+          <Button name="manufacturer" onMouseEnter={this.handleFilterEnter} onMouseLeave={this.handleFilterLeave}>Производитель</Button>
           {this.renderFilter('manufacturer')}
         </div>
         <form className="text-center">
